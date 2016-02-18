773521cff8575badc8c2052c6764d45bea643bd2
Elomar Souza <elomar@elomar.me>
2016-02-18 15:08:35 -0800

    Update battery driver to reflect updated wiring

--- rpic/BatteryDriver/BatteryStatus.ino
+++ rpic/BatteryDriver/BatteryStatus.ino
@@ -1,4 +1,3 @@
-
 /*// In the main function you need to include BatteryStatus.h:
 #include <BatteryStatus.h>
 
@@ -9,11 +8,11 @@ Needs to define arduino analog input pin in the main function
 float BatteryStatus(int analogPin) {
   int val=analogRead(analogPin);
   float volt = backMapping(val);    // Scaled output voltage
-  float BatVolt = volt*(1.8+1);       // 2 is the ratio between voltage divider resistors r1/r2
+  float BatVolt = volt*(2.819)*1.0219;       // 2 is the ratio between voltage divider resistors r1/r2
   return BatVolt;
 }
 
 float backMapping(int value){
-  float volt = value*5/1023;
+  float volt = value*5/1024;
   return volt;
   }
